259773834ed500a48e82c699b2227376d38dfeb2
huimin chen <huimin.chen@studenti.unipd.it>
2024-04-16 15:16:10 +0200

    add new function

--- src/components/component/flipCard.tsx
+++ src/components/component/flipCard.tsx
@@ -19,7 +19,7 @@ export const FlipCard: FC<ProjectCardProps> = ({
 }) => {
   return (
     <div className="flex flex-col justify-center bg-slate-100 dark:bg-slate-900">
-      <div className="group h-96 w-96 [perspective:1000px]">
+      <div className="group sm:h-96 sm:w-96 h-72 w-72 [perspective:1000px]">
         <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
           <div className="absolute inset-0">
             <img
